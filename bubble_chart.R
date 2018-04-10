95437912b2e7b55cc0b1bbccd0e746458a18be20
Shoombuatong2527 <33542325+Shoombuatong2527@users.noreply.github.com>
2018-04-10 14:14:18 +0500

    Update bubble_chart.R

--- bubble_chart.R
+++ bubble_chart.R
@@ -1,26 +1,7 @@
-
-########### Buble plot
-ggplot(data, aes(x=carat, y=price)) +
-  geom_point()
- 
-# Now we see there is a link between caract and price
-# But what if we want to know about depth in the same time?
+########### plot 2 variables and dot size depends on the value of Petal.Length
 ggplot(iris, aes(x=Sepal.Length, y=Sepal.Width,size=Petal.Length)) +
-  geom_point(alpha=0.2)
+geom_point(alpha=0.6)
  
-
-library(ggplot2)
-library(plotly)
-library(gapminder)
- 
-ggplot( iris, aes(x=Sepal.Length, y=Sepal.Width,size=Petal.Length)) +
- geom_point(alpha=0.2)
-
-
-ggplot( iris, aes(x=Sepal.Length, y=Sepal.Width,size=Species)) +
- geom_point(alpha=0.2) +
-  scale_size_continuous(range = c(0.5, 16))
-  
-  
-ggplot(iris, aes(x=Sepal.Length, y=Sepal.Width, color=Species, shape=Species)) +
-    geom_point(size=6, alpha=0.6)
+########### plot 2 variables and dot size depends on the value of Petal.Length, while each color represents each species
+ggplot( iris, aes(x=Sepal.Length, y=Sepal.Width,size=Petal.Length,color=Species,)) +
+ geom_point(alpha=0.6)
