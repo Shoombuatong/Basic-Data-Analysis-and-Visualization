22605437453685ec71b64960180f3b2ebd705c7e
Shoombuatong2527 <33542325+Shoombuatong2527@users.noreply.github.com>
2018-04-10 13:35:47 +0500

    Update scatter_plot.R

--- scatter_plot.R
+++ scatter_plot.R
@@ -1,1 +1,15 @@
-plot_ly(data = iris, x = ~Sepal.Length, y = ~Petal.Length)
+#### Basic scatterplot
+library(ggplot2)
+ggplot(iris, aes(x=Sepal.Length, y=Sepal.Width)) + 
+    geom_point()
+
+#### Custom scatterplot
+library(ggplot2)
+
+# Color and shape depend on factor (categorical variable)
+ggplot(iris, aes(x=Sepal.Length, y=Sepal.Width, color=Species, shape=Species)) +
+    geom_point(size=6, alpha=0.6)
+ 
+# Color and shape depend on factor (numerical variable)
+ggplot(iris, aes(x=Sepal.Length, y=Sepal.Width, color=Petal.Length, size=Petal.Length)) +
+    geom_point(alpha=0.6)
