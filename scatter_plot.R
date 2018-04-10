8726613ed31a75d5c2d6c61e37a0627722c722ed
Shoombuatong2527 <33542325+Shoombuatong2527@users.noreply.github.com>
2018-04-10 14:43:20 +0500

    Update scatter_plot.R

--- scatter_plot.R
+++ scatter_plot.R
@@ -4,8 +4,6 @@ ggplot(iris, aes(x=Sepal.Length, y=Sepal.Width)) +
     geom_point()
 
 #### Custom scatterplot
-library(ggplot2)
-
 ggplot(iris, aes(x=Sepal.Length, y=Sepal.Width)) + 
 geom_point(size=6, alpha=0.6)
 
