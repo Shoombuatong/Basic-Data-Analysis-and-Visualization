2494e619ab3cc99b3bc45c6c8a3b9c22bdba8795
Shoombuatong2527 <33542325+Shoombuatong2527@users.noreply.github.com>
2018-04-17 08:33:29 +0500

    Update Basic_R_programming.R

--- Basic_R_programming.R
+++ Basic_R_programming.R
@@ -1,8 +1,6 @@
 # Filename: ProgrammingBasics.R
  
 # ---Simple Calculations---
-2 + 3
- 
 x <- 2
 y <- 3
 x + y
@@ -15,10 +13,10 @@ workshop <- c(1, 2, 1, 2, 1, 2, 1, 2)
 print(workshop)
 workshop
  
-gender <- c("f", "f", "f", NA, "m", "m", "m", "m")
+gender <- c("f", "f", "f", "f", "m", "m", "m", "m")
 q1 <- c(1, 2, 2, 3, 4, 5, 5, 4)
 q2 <- c(1, 1, 2, 1, 5, 4, 3, 5)
-q3 <- c(5, 4, 4,NA, 2, 5, 4, 5)
+q3 <- c(5, 4, 4, NA, 2, 5, 4, 5)
 q4 <- c(1, 1, 3, 3, 4, 5, 4, 5)
  
 # Selecting Elements of Vectors
@@ -26,7 +24,7 @@ q1[5]
 q1[ c(5, 6, 7, 8) ]
 q1[5:8]
 q1[gender == "m"]
-mean( q1[ gender == "m" ], na.rm = TRUE)
+mean( q1[ gender == "m" ])
  
 # ---Factors---
  
