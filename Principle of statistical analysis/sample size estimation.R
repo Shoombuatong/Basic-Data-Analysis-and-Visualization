8e1df513c99a1614aa38949637b6028fc08b0382
Shoombuatong2527 <33542325+Shoombuatong2527@users.noreply.github.com>
2018-04-23 09:58:01 +0500

    Update sample size estimation.R

--- Principle of statistical analysis/sample size estimation.R
+++ Principle of statistical analysis/sample size estimation.R
@@ -8,3 +8,12 @@ n = ceiling((z^2 * s^2)/(e^2))
 return(n)
 }
 
+# Estimation population proportion (Estimation of 1 proportion)
+# z is confidence interval.
+# s is standard deviation.
+# e is error.
+
+Nepp =function(z,p,e){
+n = ceiling((z^2 *p*(1-p))/(e^2))
+return(n)
+}
