be3f4d3d76d52f2a2283c6956f701b1327bb56ab
Shoombuatong2527 <33542325+Shoombuatong2527@users.noreply.github.com>
2018-04-24 13:15:06 +0500

    Update sample size estimation.R

--- Principle of statistical analysis/sample size estimation.R
+++ Principle of statistical analysis/sample size estimation.R
@@ -3,7 +3,7 @@
 # sd is a standard deviation.
 # e is an error.
 
-NE1P =function(z,sd,e){
+NE1M =function(z,sd,e){
 n = ceiling((z^2 * sd^2)/(e^2))
 return(n)
 }
@@ -34,10 +34,11 @@ return(n)
 # Comparing 2 means independent
 # z0 is a confidence interval of a control group.
 # z1 is a confidence interval of a study grpup.
-# p0 is a proportion of a control group.
-# p1 is a proportion of a study grpup.
+# p0 is the mean of a control group.
+# p1 is the mean of a study group.
+# sd is a standard deviation.
 
-NC2M =function(z0,z1,p0,p1,sd){
+NC2M =function(z_alpha,z_beta,p0,p1,sd){
 up = (z0 + z1)*sd
 down = p1-p0
 n = ceiling((up/down)^2)
