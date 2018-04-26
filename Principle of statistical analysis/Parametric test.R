3b4fbf6ddce94de2ca55d083f99e8b045407c786
Shoombuatong2527 <33542325+Shoombuatong2527@users.noreply.github.com>
2018-04-26 12:10:29 +0500

    Update Parametric test.R

--- Principle of statistical analysis/Parametric test.R
+++ Principle of statistical analysis/Parametric test.R
@@ -50,9 +50,34 @@ bpdata
 myanova <- aov(bloodpressure ~ biofeedback*drug)
 summary(myanova)
 
+TukeyHSD(myanova,which="biofeedback:drug")
 # Ex 2 Two-way ANOVA
-D = read.csv("deliver.csv", header=T)
+delivery.df = data.frame(
+  Service = c(rep("Carrier 1", 15), rep("Carrier 2", 15),
+    rep("Carrier 3", 15)),
+  Destination = c(rep(c("Office 1", "Office 2", "Office 3",
+    "Office 4", "Office 5"), 9)),
+  Time = c(15.23, 14.32, 14.77, 15.12, 14.05,
+  15.48, 14.13, 14.46, 15.62, 14.23, 15.19, 14.67, 14.48, 15.34, 14.22,
+  16.66, 16.27, 16.35, 16.93, 15.05, 16.98, 16.43, 15.95, 16.73, 15.62,
+  16.53, 16.26, 15.69, 16.97, 15.37, 17.12, 16.65, 15.73, 17.77, 15.52,
+  16.15, 16.86, 15.18, 17.96, 15.26, 16.36, 16.44, 14.82, 17.62, 15.04)
+)
 
+myanova <- aov(Time ~ Service*Destination, data= delivery.df)
+summary(myanova)
 
+TukeyHSD(myanova,which="Service:Destination")
+
+                   
+ # Ex 2 Two-way ANOVA
+
+D = read.csv("Stree.csv", header = TRUE)
+ 
+myanova <- aov(SCORE ~ METHOD*GENDER, data= D)
+summary(myanova)
+
+TukeyHSD(myanova,which="METHOD:GENDER")
+                  
 
 
