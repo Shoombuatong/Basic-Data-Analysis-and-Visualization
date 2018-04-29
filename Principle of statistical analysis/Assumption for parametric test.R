2ca5eefd6527018bc6370e144329d1d13bcd790f
Shoombuatong2527 <33542325+Shoombuatong2527@users.noreply.github.com>
2018-04-29 07:31:33 +0500

    Create Assumption for parametric test.R

---
+++ Principle of statistical analysis/Assumption for parametric test.R
@@ -0,0 +1,5 @@
+### Assumption for one-sample t-tests
+
+# 1. The data must be continuous. 
+# 2. The data must follow the normal probability distribution. 
+# 3. The sample is a simple random sample from its population.
