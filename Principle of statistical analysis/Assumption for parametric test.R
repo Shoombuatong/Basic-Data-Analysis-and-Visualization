1bb04a58da88f7efc9f93258e509f15849116e0d
Shoombuatong2527 <33542325+Shoombuatong2527@users.noreply.github.com>
2018-04-29 09:33:28 +0500

    Update Assumption for parametric test.R

--- Principle of statistical analysis/Assumption for parametric test.R
+++ Principle of statistical analysis/Assumption for parametric test.R
@@ -4,7 +4,7 @@ drug = c(rep("A",9), rep("B",9), rep("C",9))
 migraine = data.frame(pain,drug)
 migraine
 
-############### Assumption for one-sample t-tests
+############### Checking Assumptions for one-sample t-tests
 
 # 1. The data must be continuous . 
 # 2. The data must follow the normal probability distribution.
@@ -14,7 +14,7 @@ shapiro.test(migraine[1:9,1 ])
 # 3. The sample is a simple random sample from its population.
 
 
-############### Assumption for two-sample t-tests
+############### Checking Assumptions for two-sample t-tests
 G1 = migraine[1:9,1 ]
 G2 = migraine[10:18,1 ]
 
@@ -35,16 +35,7 @@ chisq.test(table(G1and2)+0.001)
 chisq.test(table(G1and2)+0.0001)
 # 4. Both samples are simple random samples from their respective populations. 
 
-
-
-
-
-
-### Assumption One-way ANOVA assumption
-pain = c(4, 5, 4, 3, 2, 4, 3, 4, 4, 6, 8, 4, 5, 4, 6, 5, 8, 6, 6, 7, 6, 6, 7, 5, 6, 5, 5)
-drug = c(rep("A",9), rep("B",9), rep("C",9))
-migraine = data.frame(pain,drug)
-migraine
+############### Checking Assumptions for One-way ANOVA 
 
 ## 1. The experimental errors of your data are normally distributed. 
 model = aov(pain ~ drug, data=migraine)
@@ -56,3 +47,23 @@ bartlett.test(pain ~ drug, data=migraine)
 
 ## 3.Independence of samples: Each sample is randomly selected and independent 
 chisq.test(table(migraine))
+
+
+############### Checking Assumptions for Two-way ANOVA 
+
+bloodpressure <- c(158,163,173,178,168,188,183,198,178,193,186,191,196,181,176,185,190,195,200,180)
+biofeedback <- factor(c(rep("present",10),rep("absent",10)))
+drug <- factor(rep(c(rep("use",5),rep("notuse",5)),2))
+bpdata <- data.frame(bloodpressure, biofeedback, drug)
+bpdata
+
+## 1. All samples are drawn from normally distributed populations.
+shapiro.test(migraine[,1])
+
+## 2. The samples have a common variance.
+G <- factor(c(rep("G1",5),rep("G2",5),rep("G3",5),rep("G4",5)))
+bpdata_new <- data.frame(bloodpressure, G)
+bartlett.test(bloodpressure ~ G, data=bpdata_new)
+
+## 3. There are no outliers that distort the test results.
+boxplot(bloodpressure ~ G, data=bpdata_new)
