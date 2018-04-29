4d1042eda9512e685b3997d3bd4669c7a7838e1e
Shoombuatong2527 <33542325+Shoombuatong2527@users.noreply.github.com>
2018-04-29 09:38:24 +0500

    Update Parametric test.R

--- Principle of statistical analysis/Parametric test.R
+++ Principle of statistical analysis/Parametric test.R
@@ -1,93 +1,47 @@
-# Ex 1 One-sample t-tests
+######### Example of One-sample t-tests
+######### Ex 1
 x = c(0.593, 0.142, 0.329, 0.691, 0.231, 0.793, 0.519, 0.392, 0.418)
 t.test(x, alternative="greater", mu=0.3,conf.level = 0.95)
 
-
-# Ex 1 Two-sample t-tests (Pooled variance)
+######### Example of Two-sample t-tests
+######### Ex 1 Two-sample t-tests (Pooled variance)
 Control = c(91, 87, 99, 77, 88, 91)
 Treat = c(101, 110, 103, 93, 99, 104)
 t.test(Control,Treat,alternative="less", var.equal=TRU,conf.level = 0.95)
 
-
-# Ex 2 Two-sample t-tests (Pooled variance)
+######### Ex 2 Two-sample t-tests (Pooled variance)
 WardA =  c(12.2, 11.1, 14.0, 11.3, 10.8, 12.5, 12.2, 11.9, 13.6, 12.7, 13.4, 13.7)
 WardB =  c(11.9, 10.7, 12.3, 13.9, 11.1, 11.2, 13.3, 11.4, 12.0, 11.1)
 t.test(WardA,WardB,alternative="two.sided", var.equal=TRUE,conf.level = 0.95)
 
-
-# Ex 2 Two-sample t-tests (Separated variance)
+######### Ex 3 Two-sample t-tests (Separated variance)
 Standard =  c(35, 104, 27, 53, 72, 64, 97, 121, 86, 41 )
 New =  c(27, 52, 46, 33, 37, 82, 51, 92, 68, 62 )
 t.test(Standard,New,alternative="two.sided", var.equal=TRUE)
 
-# Check One-way ANOVA assumption
+######### Example of One-way ANOVA assumption
+######### Ex 1
 pain = c(4, 5, 4, 3, 2, 4, 3, 4, 4, 6, 8, 4, 5, 4, 6, 5, 8, 6, 6, 7, 6, 6, 7, 5, 6, 5, 5)
 drug = c(rep("A",9), rep("B",9), rep("C",9))
 migraine = data.frame(pain,drug)
 migraine
 
-## 1. The experimental errors of your data are normally distributed. 
-model = aov(pain ~ drug, data=migraine)
-model$residuals
-shapiro.test(model$residuals)
-
-## 2. Equal variances between treatments Homogeneity of variances Homoscedasticity. 
-bartlett.test(pain ~ drug, data=migraine)
-
-## 3.Independence of samples: Each sample is randomly selected and independent 
-chisq.test(table(migraine))
-
-## Analysis result
 aov.out = aov(pain ~ drug, data=migraine)
 summary(aov.out)
 TukeyHSD(aov.out)
 
-
-# Ex 2 One-way ANOVA
+######### Ex 2 One-way ANOVA
 attach(InsectSprays)
 InsectSprays
 plot(count ~ spray, data=InsectSprays)
 
-## 1. The experimental errors of your data are normally distributed. 
-model = aov(count ~ spray, data=InsectSprays)
-model$residuals
-shapiro.test(model$residuals)
-
-## 2. Equal variances between treatments Homogeneity of variances Homoscedasticity. 
-bartlett.test(count ~ spray, data=InsectSprays)
-
-## 3.Independence of samples: Each sample is randomly selected and independent 
-chisq.test(table(InsectSprays))
-
-## Analysis result
 aov.out = aov(count ~ spray, data=InsectSprays)
 summary(aov.out)
 TukeyHSD(aov.out)
 
 
-# Check Two-way ANOVA assumption
-bloodpressure <- c(158,163,173,178,168,188,183,198,178,193,186,191,196,181,176,185,190,195,200,180)
-biofeedback <- factor(c(rep("present",10),rep("absent",10)))
-drug <- factor(rep(c(rep("use",5),rep("notuse",5)),2))
-bpdata <- data.frame(bloodpressure, biofeedback, drug)
-bpdata
-## 1. All samples are drawn from normally distributed populations.
-model = aov(pain ~ drug, data=migraine)
-model$residuals
-shapiro.test(model$residuals)
-
-## 2. The samples have a common variance.
-bartlett.test(pain ~ drug, data=migraine)
-
-## 3. There are no outliers that distort the test results.
-G <- factor(c(rep("G1",5),rep("G2",5),rep("G3",5),rep("G4",5)))
-bpdata_new <- data.frame(bloodpressure, G)
-
-model = aov(bloodpressure ~ G, data=bpdata_new)
-model$residuals
-
-
-# Ex 1 Two-way ANOVA
+######### Example of Two-way ANOVA assumption
+######### Ex 1 Two-way ANOVA
 bloodpressure <- c(158,163,173,178,168,188,183,198,178,193,186,191,196,181,176,185,190,195,200,180)
 biofeedback <- factor(c(rep("present",10),rep("absent",10)))
 drug <- factor(rep(c(rep("use",5),rep("notuse",5)),2))
@@ -99,7 +53,7 @@ summary(myanova)
 
 TukeyHSD(myanova,which="biofeedback:drug")
 
-# Ex 2 Two-way ANOVA
+######### Ex 2 Two-way ANOVA
 delivery.df = data.frame(
   Service = c(rep("Carrier 1", 15), rep("Carrier 2", 15),
     rep("Carrier 3", 15)),
@@ -117,13 +71,10 @@ summary(myanova)
 
 TukeyHSD(myanova,which="Service:Destination")
                    
- # Ex 3 Two-way ANOVA
+######### Ex 3 Two-way ANOVA
 D = read.csv("Stree.csv", header = TRUE)
  
 myanova <- aov(SCORE ~ METHOD*GENDER, data= D)
 summary(myanova)
 
 TukeyHSD(myanova,which="METHOD:GENDER")
-                  
-
-
