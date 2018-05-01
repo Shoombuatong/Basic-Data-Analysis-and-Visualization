ae93b324e4922a161e00be1c108eb98747efc4c8
Shoombuatong2527 <33542325+Shoombuatong2527@users.noreply.github.com>
2018-05-01 12:08:22 +0500

    Delete Decision tree.R

--- Decision tree.R
+++
@@ -1,9 +0,0 @@
-############ Set directory
-setwd('D:\\Workshop')
-dataset = read.csv("Diabetes.csv", header=T)
-
-############ Plot decision tree
-library(party)
-tree = ctree(class ~., data = dataset)
-plot(tree)
-table(predict(tree), dataset$class)
