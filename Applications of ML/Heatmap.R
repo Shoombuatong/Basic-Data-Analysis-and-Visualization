1d7e082990e81f944cf25e7a3e0436fb5e72c115
Shoombuatong2527 <33542325+Shoombuatong2527@users.noreply.github.com>
2018-05-04 07:39:19 +0500

    Update Heatmap.R

--- Applications of ML/Heatmap.R
+++ Applications of ML/Heatmap.R
@@ -5,7 +5,20 @@ D = read.csv("gene-expression.csv", header=T)
 label = read.csv("labels.csv", header=T)
 dataset = D[,-1]
 
+##### Simple heatmap
 Dselect = dataset[1:50,1:50]
 df <- Dselect[, ! apply(Dselect , 2 , function(x) sd(x) < 0.9 ) ]
 nDat = scale(df)
 heatmap(nDat, scale = "none")
+
+ ##### Adding row name                                              
+row.names(nDat) = as.character(label[1:50,1])                        
+heatmap(nDat)                        
+
+ ##### Adding row name                         
+library("pheatmap")
+pheatmap(nDat, cutree_rows = 2)
+pheatmap(nDat, cutree_rows = 3)
+                        
+                        
+                        
