9ad6c996af298c9719f7ded6a5b7083ee6751489
Shoombuatong2527 <33542325+Shoombuatong2527@users.noreply.github.com>
2018-05-21 14:01:16 +0500

    Create Nonparametric Tests.R

---
+++ Principle of statistical analysis/Nonparametric Tests.R
@@ -0,0 +1,7 @@
+women_weight <- c(38.9, 61.2, 73.3, 21.8, 63.4, 64.6, 48.4, 48.8, 48.5)
+men_weight <- c(67.8, 60, 63.4, 76, 89.4, 73.3, 67.3, 61.3, 62.4) 
+# Create a data frame
+my_data <- data.frame( 
+                group = rep(c("Woman", "Man"), each = 9),
+                weight = c(women_weight,  men_weight)
+                )
