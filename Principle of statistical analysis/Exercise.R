2981a6cbbe012e2ab76c5dfd98ce5a742ad32c60
Shoombuatong2527 <33542325+Shoombuatong2527@users.noreply.github.com>
2018-07-07 13:27:57 +0500

    Update Exercise.R

--- Principle of statistical analysis/Exercise.R
+++ Principle of statistical analysis/Exercise.R
@@ -1,12 +1,11 @@
+#### Normality test
 #### In case of w2
 hist(w2, col="grey")
 hist(w2, col="grey", freq=F)
 
 curve( dnorm(x, mean = mean(w2),sd= sd(w2)), add=T, col="red")
 
-ggdensity(w2, 
-          main = "W1",
-          xlab = "value")
+ggdensity(w2, main = "W1", xlab = "value")
 
 qqnorm(w2)
 qqline(w2, col = 2)
