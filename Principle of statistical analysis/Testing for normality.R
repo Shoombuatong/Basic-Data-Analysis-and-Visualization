5f3f25d1cbb84737a168a3846da6996742b924cc
Shoombuatong2527 <33542325+Shoombuatong2527@users.noreply.github.com>
2018-07-07 12:36:31 +0500

    Update Testing for normality.R

--- Principle of statistical analysis/Testing for normality.R
+++ Principle of statistical analysis/Testing for normality.R
@@ -20,7 +20,6 @@ ggdensity(w1,
           xlab = "value")
 
 ## Plot using a qqplot
-par( mfrow = c(1,2 ))
 qqnorm(w1)
 qqline(w1, col = 2)
 
@@ -28,31 +27,6 @@ ggqqplot(w1)
 
 ## Perform the test
 shapiro.test(w1)
-
-#### In case of w2
-hist(w2, col="grey")
-hist(w2, col="grey", freq=F)
-
-curve( dnorm(x, mean = mean(w2),sd= sd(w2)), add=T, col="red")
-
-ggdensity(w2, 
-          main = "W1",
-          xlab = "value")
-
-qqnorm(w2)
-qqline(w2, col = 2)
-
-ggqqplot(w2)
-
-shapiro.test(w2)
-
-## Plot multi qqplot
-par( mfrow = c(1,2 ),mai=c(0.3,0.3,0.15,0.15))
-qqnorm(w1)
-qqline(w1, col = 2)
-
-qqnorm(w2)
-qqline(w2, col = 2)
 
 ## eGFR example
 setwd('D:\\workshop')
