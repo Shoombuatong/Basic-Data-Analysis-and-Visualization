8812bc4fa3f68fb92df4346132f43e84e5dcb161
Shoombuatong2527 <33542325+Shoombuatong2527@users.noreply.github.com>
2018-07-08 08:56:06 +0500

    Update Exercise.R

--- Principle of statistical analysis/Exercise.R
+++ Principle of statistical analysis/Exercise.R
@@ -1,4 +1,4 @@
-#### Normality test
+####################### Normality test
 #### In case of w2
 hist(w2, col="grey")
 hist(w2, col="grey", freq=F)
@@ -20,7 +20,7 @@ qqline(w1, col = 2)
 qqnorm(w2)
 qqline(w2, col = 2)
 
-
+#######################Nonparametric Tests
 ### Mann Whitney U Test (Wilcoxon Rank Sum Test)
 ### Ex. 2
 usual	= c(8,	7,	6,	2,	5,	8,	7,	3)
@@ -32,3 +32,11 @@ wilcox.test(usual	,new , alternative = c("two.sided"), exact = FALSE,paired=FALS
 brandA = c(24,27,19,24,22,26,27,25,22)
 brandB = c(26,27,22,27,25,27,26,27,23)
 wilcox.test(brandA, brandB, paired=TRUE,alternative = c("two.sided"), exact = FALSE) 
+
+### The Kruskal-Wallis Test
+salary = c(23, 41, 54, 66, 78, 45, 55, 60, 70, 72, 18, 30, 34, 40, 44)
+group = c(rep("Women",5), rep("Men",5), rep("Minorities",5))
+shoes = data.frame(salary,group)
+
+kruskal.test(salary ~ group, data = shoes)
+posthoc.kruskal.nemenyi.test(salary ~ group, data = shoes, method="Tukey")
