90bb126a4399ea4e77a710ab6515147498260fbe
Shoombuatong2527 <33542325+Shoombuatong2527@users.noreply.github.com>
2018-07-11 14:36:41 +0500

    Update Plotting data.R

--- Data visualization/Plotting data.R
+++ Data visualization/Plotting data.R
@@ -132,8 +132,6 @@ drugB  = subset(migraine, drug == "B")
 drugC  = subset(migraine, drug == "C")
 
 par( mfrow = c(2,2 ),mai=c(0.5,0.5,0.4,0.4))
-boxplot(pain ~ drug,data= migraine, main='Migraine', col = colors ,horizontal=FALSE)
-
 hist(drugA[,1],main="Drug A vs. Drug B",ylab="Frequency",xlab="TG",col=rgb(1,0,0,0.7),breaks=10) 
 hist(drugB[,1],main=NA,ylab=NULL,xlab=NULL,col=rgb(0,0,1,0.4),breaks=10, add=T) 
 
@@ -143,6 +141,34 @@ hist(drugC[,1],main=NA,ylab=NULL,xlab=NULL,col=rgb(0,0,1,0.4),breaks=5, add=T)
 hist(drugB[,1],main="Drug B vs. Drug C",ylab="Frequency",xlab="TG",col=rgb(1,0,0,0.7),breaks=5) 
 hist(drugC[,1],main=NA,ylab=NULL,xlab=NULL,col=rgb(0,0,1,0.4),breaks=5, add=T) 
 
+########################### multiplot
+pain = c(4, 5, 4, 3, 2, 4, 3, 4, 4, 6, 8, 4, 5, 4, 6, 5, 8, 6, 6, 7, 6, 6, 7, 5, 6, 5, 5)
+drug = c(rep("A",9), rep("B",9), rep("C",9))
+migraine = data.frame(pain,drug)
+colors <- c("red","blue","green")
+boxplot(pain ~ drug,data= migraine, main='Migraine', col = colors ,horizontal=FALSE)
+
+drugA  = subset(migraine, drug == "A")
+drugB  = subset(migraine, drug == "B")
+drugC  = subset(migraine, drug == "C")
+drugAB = rbind(drugA, drugB)
+drugAC = rbind(drugA, drugC)
+drugBC = rbind(drugB, drugC)
+
+par( mfrow = c(2,2 ),mai=c(0.5,0.5,0.4,0.4))
+boxplot(pain ~ drug,data= migraine, main='Migraine', col = colors ,horizontal=FALSE)
+boxplot(pain ~ drug,data= drugAB, main='Migraine', col = c("red","blue") ,horizontal=FALSE)
+boxplot(pain ~ drug,data= drugAC, main='Migraine', col = c("red","green") ,horizontal=FALSE)
+boxplot(pain ~ drug,data= drugBC, main='Migraine', col = c("blue","green") ,horizontal=FALSE)
 
 
+library(base)
+drugAB$drug <- factor(drugAB$drug)
+drugAC$drug <- factor(drugAC$drug)
+drugBC$drug <- factor(drugBC$drug)
 
+par( mfrow = c(2,2 ),mai=c(0.5,0.5,0.4,0.4))
+boxplot(pain ~ drug,data= migraine, main='Migraine', col = colors ,horizontal=FALSE)
+boxplot(pain ~ drug,data= drugAB, main='Migraine', col = c("red","blue") ,horizontal=FALSE)
+boxplot(pain ~ drug,data= drugAC, main='Migraine', col = c("red","green") ,horizontal=FALSE)
+boxplot(pain ~ drug,data= drugBC, main='Migraine', col = c("blue","green") ,horizontal=FALSE)
