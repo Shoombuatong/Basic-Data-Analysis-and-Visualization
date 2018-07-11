7b4ebed4d0cd02515cb320ed7190049f1ec7b126
Shoombuatong2527 <33542325+Shoombuatong2527@users.noreply.github.com>
2018-07-11 16:12:58 +0500

    Update Exercise.R

--- Principle of statistical analysis/Exercise.R
+++ Principle of statistical analysis/Exercise.R
@@ -40,6 +40,17 @@ aov.out = aov(pain ~ drug, data=migraine)
 summary(aov.out)
 TukeyHSD(aov.out)
 
+######### Example of Two-way ANOVA assumption
+relief = c(12,15,16,17,14,14,17,19,20,17,25,27,29,24,22,21,19,18,24,25,21,20,23,27,25,37,34,36,26,29)
+sex <- factor(c(rep("male",15),rep("female",15)))
+treatment <- factor(rep(c(rep("A",5),rep("B",5),rep("C",5)),2))
+data <- data.frame(relief, sex , treatment)
+data
+
+myanova <- aov(relief ~ sex*treatment)
+summary(myanova)
+TukeyHSD(myanova,which="sex:treatment")
+
 ####################### Nonparametric Tests
 ######### Mann Whitney U Test (Wilcoxon Rank Sum Test)
 usual	= c(8,	7,	6,	2,	5,	8,	7,	3)
